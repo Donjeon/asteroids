20851f60b67e8d78e7fde111899f4f2408134d74
Donjeon <hasturburialteam@pm.me>
2025-01-05 15:04:27 +0000

    Set screen to populate and added FPS counter

--- main.py
+++ main.py
@@ -3,22 +3,34 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    py_clock = pygame.time.Clock()
+    
+    dt = 0
+    frame_rate = 60
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        
-        screen.fill("yellow")
+        screen.fill("black")
+        fps_counter(screen, py_clock)
         pygame.display.flip()
+        
+        dt = (py_clock.tick(frame_rate))/1000 #Binding game to FPS
+        
 
+def fps_counter(display_screen, game_clock):
+    fps_font =  pygame.font.Font(None, 36)
     
+    fps = str(int(game_clock.get_fps()))
+    fps_t = fps_font.render(fps , 1, pygame.Color("RED"))
+    display_screen.blit(fps_t,(0,0))
+
 
 if __name__ == "__main__":
     main()
